6595952037e3b230f717b6924093406f86525abc
drops <coco554211@gmail.com>
2021-08-04 15:10:29 +0800

    Initial commit

---
+++ cogs/reaction.py
@@ -0,0 +1,61 @@
+from discord.ext import commands
+from zhconv import convert
+import random
+import time
+import discord
+import os
+import jieba
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+REACTIONS = [
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335866331295744/LmVdMYT.jpg',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335866662649876/C_Chat.M.1595128807.A.E19.jpg',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335869820940308/bx8C06k.png',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335872639504414/iYBXvZg.jpg',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335873646166046/iDLV0L8.jpg',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335874396938290/Ja8PdF5.png',
+    f'https://cdn.discordapp.com/attachments/870138830949322783/872335874971553828/7mLrCNah.jpg'
+]
+
+with open(os.path.join(__location__, "chinaword.txt"), "r", encoding="utf-8") as f:
+    CHINA_WORD = f.read().splitlines()
+
+class reaction(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        random.seed(int(time.time()))
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        messageContent = msg.content
+        converted_message = convert(messageContent, 'zh-hant')
+        seg_list = jieba.cut(converted_message)
+        for seg in seg_list:
+            if seg in CHINA_WORD:
+                await msg.add_reaction('<:zu2:815557862528122890>')
+                author = msg.author.id
+                if msg.author.bot:
+                    break
+                await msg.channel.send(f'<@{author}> 支語，滾！')
+                mesg = random.choice(REACTIONS)
+                await msg.channel.send(mesg)
+                break
+
+    @commands.command()
+    async def update_word(self, ctx, arg = None):
+        if arg == None:
+            await ctx.channel.send('usage: $update_word <zhi yu>')
+            return
+        if arg in CHINA_WORD:
+            await ctx.channel.send('親 這個支語已被收錄啦哈')
+            return
+        with open(os.path.join(__location__, "chinaword.txt") , "a", encoding="utf-8") as china:
+            china.write('\n' + arg)
+            await ctx.channel.send('親 已經為您更新支語資料庫啦哈')
+        with open(os.path.join(__location__, "chinaword.txt"), "r", encoding="utf-8") as f:
+            CHINA_WORD[:] = f
+
+def setup(bot):
+    bot.add_cog(reaction(bot))
---
+++ index.py
@@ -0,0 +1,46 @@
+from discord.ext import commands
+import discord
+import json
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, 'config.json'),'r',encoding="utf8") as jfile:
+    jdata = json.load(jfile)
+
+bot = commands.Bot(command_prefix=jdata['prefix'], help_command=None)
+
+@bot.event
+async def on_ready():
+    print('{0.user}起床囉'.format(bot))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="支語殺"))
+    
+    print('Servers connected to:')
+    for guild in bot.guilds:
+        print(guild.name)
+
+@bot.command(pass_context = True)
+@commands.is_owner()
+async def load(ctx, ext):
+    await ctx.send(f'{ext} loaded')
+    await bot.load_extension(f'cogs.{ext}')
+
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, ext):
+    await ctx.send(f'{ext} unloaded')
+    await bot.unload_extension(f'cogs.{ext}')
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, ext):
+    await ctx.send(f'{ext} reloaded')
+    await bot.reload_extension(f'cogs.{ext}') 
+
+for filename in os.listdir(os.path.join(__location__, 'cogs')):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+bot.run(jdata['token'])
